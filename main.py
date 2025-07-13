60388db163f2c4904a66b7572282119da78695bd
ᴊᴀᴛɪɴꜱʜᴀʀᴍᴀ <radhakrishnaj701@gmail.com>
2025-07-13 14:03:06 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the Table".center(50, "*"))
+num = int(input("Enter the number: "))
+
+for i in range(1, 11):
+    print(f"{num} X {i} = {num * i}")
+
+print("I always Run")
+print("Thanks for using the table")
